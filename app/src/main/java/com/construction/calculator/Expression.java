ced1ca2ae7482c6b1e09e8b2ee451b82f29b6711
Shane Cunningham <mayhem2277@gmail.com>
2018-11-18 11:33:18 +0000

    added empty activitiy

---
+++ app/src/main/java/com/construction/calculator/Expression.java
@@ -0,0 +1,15 @@
+package com.construction.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.calculator.R;
+
+public class Expression extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expression);
+    }
+}
