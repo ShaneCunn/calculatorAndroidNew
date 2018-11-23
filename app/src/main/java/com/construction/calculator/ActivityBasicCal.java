75a8138f962948c824e0f7816f2dd74e372c8524
Shane Cunningham <mayhem2277@gmail.com>
2018-11-23 12:48:00 +0000

    Added basic calculator

---
+++ app/src/main/java/com/construction/calculator/ActivityBasicCal.java
@@ -0,0 +1,13 @@
+package com.construction.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityBasicCal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_cal);
+    }
+}
